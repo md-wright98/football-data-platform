2a3de538d64cdbaf4aa33a82a71568a9ca74b06d
Michael Wright <michaeldavidwright@outlook.com>
2026-05-08 09:54:14 +0100

    Deleted test scripts and script to create landing table. Refactored main.py, pipeline script and script to create raw data table.

--- scripts/main.py
+++ scripts/main.py
@@ -1,14 +1,28 @@
 from match_data_pipeline import download_match_data, transform_match_data, load_match_data_to_bigquery 
 import argparse
+import logging
 
 dataset_id = "epl_raw"
-table_id = "football-data-platform-483422.epl_raw.understat_matches_landing"
+table_id = "football-data-platform-483422.epl_raw.understat_matches_raw"
 
 def main(league: str, season: str):
+  
+  try:
 
-  raw_match_data, extracted_at = download_match_data(league=league, season=season)
-  transformed_data = transform_match_data(data=raw_match_data, league=league, season=season, extracted_at_timestamp=extracted_at)
-  load_match_data_to_bigquery(transformed_data, table_id)
+    raw_match_data, extracted_at = download_match_data(league=league, season=season)
+
+    transformed_data = transform_match_data(
+      data=raw_match_data, 
+      league=league, 
+      season=season, 
+      extracted_at_timestamp=extracted_at)
+    
+    load_match_data_to_bigquery(transformed_data, table_id)
+
+    logging.info(f"Successfully completed pipeline run for {league} {season}")
+  
+  except Exception as e:
+    logging.error(f"Pipeline failed for {league} {season}: {str(e)}")
 
 if __name__ == "__main__":
 
@@ -30,6 +44,6 @@ if __name__ == "__main__":
     help="Season to fetch data for. Must be the first year of the season, as a string (default: 2025)."
   )
 
-args = parser.parse_args()
+  args = parser.parse_args()
 
-main(league=args.league, season=args.season)
+  main(league=args.league, season=args.season)
