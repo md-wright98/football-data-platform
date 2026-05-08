dba44bb860dc09bbe44b63fecfc77d540d6dbcae
Michael Wright <michaeldavidwright@outlook.com>
2026-05-08 09:36:34 +0000

    Add stg_understat__matches.sql and _stg_understat.yml schema file

--- models/staging/stg_understat__matches.sql
+++ models/staging/stg_understat__matches.sql
@@ -1,3 +1,7 @@
+{{ config(
+    materialized='table'
+) }}
+
 with raw as (
 
     select * from {{ source('understat', 'understat_matches_raw') }}
@@ -15,17 +19,17 @@ flattened as (
         json_value(payload, "$.a.id") AS away_team_id,
         json_value(payload, "$.a.title") AS away_team_name,
         json_value(payload, "$.a.short_title") AS away_team_name_short,
-        json_value(payload, "$.goals.h") as home_goals,
-        json_value(payload, "$.goals.a") as away_goals,
-        json_value(payload, "$.xG.h") as home_xg,
-        json_value(payload, "$.xG.a") as away_xg,
+        safe_cast(json_value(payload, "$.goals.h") as int64) as home_goals,
+        safe_cast(json_value(payload, "$.goals.a") as int64) as away_goals,
+        safe_cast(json_value(payload, "$.xG.h") as float64) as home_xg,
+        safe_cast(json_value(payload, "$.xG.a") as float64) as away_xg,
         cast(timestamp(json_value(payload, '$.datetime')) as date) as match_date,
         cast(timestamp(json_value(payload, '$.datetime')) as time) as match_time,
-        json_value(payload, "$.forecast.w") as home_win_probability,
-        json_value(payload, "$.forecast.d") as draw_probability,
-        json_value(payload, "$.forecast.l") as away_win_probability,
+        safe_cast(json_value(payload, "$.forecast.w") as float64) as home_win_probability,
+        safe_cast(json_value(payload, "$.forecast.d") as float64) as draw_probability,
+        safe_cast(json_value(payload, "$.forecast.l") as float64) as away_win_probability,
     from raw
-    qualify row_number() over (partition by match_id order by extracted_at desc)
+    qualify row_number() over (partition by match_id order by extracted_at desc) = 1
 
 )
 
